5de323a650d6b58c2a1f6649a0e681989f457690
DiscordWizard <pausakime.if@gmail.com>
2023-05-15 15:03:51 +0200

    Initial Commit

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+ 'name': "Zoo",
+ 'author': 'Isaac Falcón Turñé',
+ 'license': "AGPL-3",
+ 'website': "",
+ 'category': 'Uncategorized',
+ 'version': '11.0.1.0',
+ 'depends': [],
+ 'data': ['views/views.xml'],
+}
